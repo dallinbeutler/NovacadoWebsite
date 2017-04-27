855458fa7b2815ee95b0d292e3d0fd22b4c2c0e7
Dallin Beutler <dallinbeutler@gmail.com>
2017-04-27 10:48:57 -0600

    init commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<HTML>
+   <head>
+      <link rel="stylesheet" type="text/css" href="mystyle.css">
+      <title> Novacado Video Player </title>
+   </head>
+   <body>
+      <div id="header">
+         <ul>
+            <li>Home</li>
+            <li> About</li>
+         </ul>
+      </div>
+   </body>
+<?php
+
+  phpinfo();
+
+?>
+
+</HTML>
