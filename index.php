64fc14970f7a9cd3d5ca9cb5f7e0a480afdb4164
dallinbeutler <dallinbeutler@gmail.com>
2017-04-27 20:34:21 -0600

    bgcolor

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	  <div class="centerArea">
 	     <div id="mainArea">
 			<div id ="pictureHolder">
-	           <img src="novacado_1.png" alt="avacados" class="centered" style="height:300px;">
+	           <img src="novacado_1.png" alt="avacados" class="centered" style="width:100%">
 			<div>
             <br/>			
 			<myp>
