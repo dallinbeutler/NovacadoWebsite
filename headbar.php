46e06ede0427eecac35bba52b649db21b3c43fd5
dallinbeutler <dallinbeutler@gmail.com>
2017-04-28 12:45:56 -0600

    removing inline styles

--- headbar.php
+++ headbar.php
@@ -4,7 +4,7 @@
                <td><a  href="index.php"><img src="novacado_pit.png" alt="avacados" class="iconsize"></a></td>
 			   <td><a href="assignList.php">Assignments</a></td>
 			   <td><a href="Download.php">Download</a></td>
-               <td><a href="edits.php">Edits</a></td>
+               <td><a href="Edits.php">Edits</a></td>
 			   <td><a href="FAQs.php">FAQs</a></td>
 			   <td><a href="about.php">About</a></td>
 			</tr>
