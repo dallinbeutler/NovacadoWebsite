b11a0166fddd4c5361f221d7673d4995980cfb1d
dallinbeutler <dallinbeutler@gmail.com>
2017-04-28 12:43:14 -0600

    php move

--- headbar.php
+++ headbar.php
@@ -2,11 +2,11 @@
          <table>
 			<tr>
                <td><a  href="index.php"><img src="novacado_pit.png" alt="avacados" class="iconsize"></a></td>
-			   <td><a href="assignList.html">Assignments</a></td>
-			   <td><a>Download</a></td>
-               <td><a>Edits</a></td>
-			   <td><a>FAQs</a></td>
-			   <td><a>About</a></td>
+			   <td><a href="assignList.php">Assignments</a></td>
+			   <td><a href="Download.php">Download</a></td>
+               <td><a href="edits.php">Edits</a></td>
+			   <td><a href="FAQs.php">FAQs</a></td>
+			   <td><a href="about.php">About</a></td>
 			</tr>
          </table>
       </div>
