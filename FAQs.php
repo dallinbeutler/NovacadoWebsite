9be80a9c95566aba3984f63dba6bdcedfe13bc6e
dallinbeutler <dallinbeutler@gmail.com>
2017-04-28 13:07:39 -0600

    php move

--- FAQs.php
+++ FAQs.php
@@ -18,25 +18,29 @@
 					 <br/><br/>
 					 A. A media player for DVD's and other video files. 
 					 It has the unique ability to censor out uncomfortable visuals, audio, or entire scenes specified by the user.
-					 <br/><br/><br/>
+				  </li><br/><br/><br/>
+				  <li>
 					 Q. Does it automatically censor things for me?
 					 <br/><br/>
 					 A. No, sadly. You must find a censor file from our handy <a class="white" href="Edits.php">online database</a> 
 					 or make your own using the built in editor.
-					 <br/><br/><br/>
+				  </li> <br/><br/><br/>
+				  <li>
 					 Q. Does it cost any money?
 					 <br/><br/>
 					 A. It is completely free. In order to keep the servers and development up and running,
 					 <a class="white" href="about.php">donations</a> would be greatly appreciated.
-					 <br/><br/><br/>
+				  </li><br/><br/><br/>
+				  <li>
 					 Q. Can I combine Censor files?
 					 <br/><br/>
 					 A. Yes. Simply choose the Edits> add Edits button in the menu for every file you want
-					 <br/><br/><br/>
+			      </li><br/><br/><br/>
+				  <li>
 					 Q. When is the full version coming out?
 					 <br/><br/>
 					 A. As soon as I feel comfortable with the product. I will try and keep those interested as informed as I can.
-					 <br/><br/><br/>
+				  </li><br/><br/><br/>
 			      </li>
 			   </ul>
 			</myp>
