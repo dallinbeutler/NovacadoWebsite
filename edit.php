b5cbe7b14eb7ab3a54fc482c826e534ea0977e70
dallinbeutler <dallinbeutler@gmail.com>
2017-05-07 09:11:51 -0600

    hw3

--- edit.php
+++ edit.php
@@ -7,10 +7,10 @@ function makeEdit($type, $number, $rating) {
 	echo $type;
 	echo '" />';
 	echo $type;
-	echo '<myp>';
+	echo '<p style="margin:auto">';
 	echo $number;
 	echo ' edits';
-	echo '</myp>';
+	echo '</p>';
 	for ($i = 0; $i < $rating; $i++){
 		echo '<img src="novacado_pit.png" alt="avacados" class="iconsize">';
 	}
