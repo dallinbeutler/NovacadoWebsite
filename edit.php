8162909cc2b483756c9c7733712cc36a84428aff
dallinbeutler <dallinbeutler@gmail.com>
2017-05-06 19:15:23 -0600

    hw3

--- edit.php
+++ edit.php
@@ -2,13 +2,13 @@
 <?php 
 
 function makeEdit($type, $number, $rating) {
-	echo '<div class= "row" >';
+	echo '<div class= "edit" >';
 	echo "<input type=\"checkbox\" name=\"$type\">";
 	echo $type;
 	echo "   ";
 	echo $number;
 	echo ' edits';
-	for ($i = 0; $i <= $rating; $i++){
+	for ($i = 0; $i < $rating; $i++){
 		echo '<img src="novacado_pit.png" alt="avacados" class="iconsize">';
 	}
     echo '</div><br/>';
