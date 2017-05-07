4e411b5c70123894348e22860dc06381290bba71
dallinbeutler <dallinbeutler@gmail.com>
2017-05-06 20:03:26 -0600

    hw3

--- edit.php
+++ edit.php
@@ -3,7 +3,7 @@
 
 function makeEdit($type, $number, $rating) {
 	echo '<div class= "edit" >';
-	echo '<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">';
+	echo '<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>';
 	echo '<input type="submit" value="Add to my edits" class="btnAddAction" />';
 	echo $type;
 	echo "   ";
