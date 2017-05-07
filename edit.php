dbf3a428a4f47ed9fb8713f581e6281082928774
dallinbeutler <dallinbeutler@gmail.com>
2017-05-06 19:00:20 -0600

    hw3

---
+++ edit.php
@@ -0,0 +1,17 @@
+
+<?php 
+
+function makeEdit($type, $number, $rating) {
+	echo '<div class= "row" >';
+	echo "input type=\"checkbox\" name=\"$type\"";
+	echo $type
+	echo "   ";
+	echo $number;
+	echo ' edits';
+	for ($i = 0; i <= rating; $i++){
+		echo " ! ";
+	}
+    echo '</div><br/>';
+}
+?>
+
