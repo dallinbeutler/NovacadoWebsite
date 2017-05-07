b277deebaa9a7130ae5d21ec309e6d8b7631929f
dallinbeutler <dallinbeutler@gmail.com>
2017-05-06 20:43:03 -0600

    hw3

--- edit.php
+++ edit.php
@@ -3,8 +3,9 @@
 
 function makeEdit($type, $number, $rating) {
 	echo '<div class= "edit" >';
-	echo '<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>';
-	echo '<input type="submit" value="Add to my edits" class="btnAddAction" />';
+	echo '<input type="checkbox" name="';
+	echo $type;
+	echo '"class="fancycheck" />';
 	echo $type;
 	echo "   ";
 	echo $number;
@@ -12,7 +13,7 @@ function makeEdit($type, $number, $rating) {
 	for ($i = 0; $i < $rating; $i++){
 		echo '<img src="novacado_pit.png" alt="avacados" class="iconsize">';
 	}
-    echo '</form></div>';
+    echo '</div>';
 }
 ?>
 
