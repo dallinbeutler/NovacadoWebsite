c769dd26de8329c9cc40160a284af2dc15595959
Dallin Beutler <dallinbeutler@gmail.com>
2017-05-22 15:37:23 -0600

    http

--- assign5.php
+++ assign5.php
@@ -23,7 +23,23 @@
 				// Check connection
 				
 				$query = 'SELECT * FROM account';
-				$result = pg_query($query);// or die('Query failed: ' . pg_last_error())
+				$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+				
+				// Printing results in HTML
+				echo "<table>\n";
+				while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+					echo "\t<tr>\n";
+					foreach ($line as $col_value) {
+						echo "\t\t<td>$col_value</td>\n";
+					}
+					echo "\t</tr>\n";
+				}
+				echo "</table>\n";
+
+				// Free resultset
+				pg_free_result($result);
+
+				
 				
 				$conn->close();
 			?>
