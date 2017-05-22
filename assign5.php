b73bd93b25b72c4a13b6cd73e50564ebc2bb1c4c
Dallin Beutler <dallinbeutler@gmail.com>
2017-05-22 15:26:10 -0600

    http

--- assign5.php
+++ assign5.php
@@ -21,7 +21,10 @@
 				//$conn = new mysqli($servername, $username, $password, $dbname);
 				$conn = pg_connect( "host=http://ec2-23-21-169-238.compute-1.amazonaws.com port=5432 dbname=dmns5jadj6q0l user=fmtextbjvwjlcy password= 6ac6980946253a82ad6759afe6c2828659ca889e406e9afeeacd53d34283a17c");
 				// Check connection
-
+				
+				$query = 'SELECT * FROM account';
+				$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+				
 				$conn->close();
 			?>
 		 </div>
