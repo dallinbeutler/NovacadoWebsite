8e7675ef2bfb335141304f744d8ade58b537f665
dallinbeutler <dallinbeutler@gmail.com>
2017-05-28 14:10:49 -0600

    saddurday

--- getmovieinfo.php
+++ getmovieinfo.php
@@ -1,19 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-table {
-    width: 100%;
-    border-collapse: collapse;
-}
 
-td, th {
-    border: 1px solid black;
-    padding: 5px;
-}
-
-th {text-align: left;}
-</style>
 </head>
 <body>
 <?php
