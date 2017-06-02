4e190ba2bc4e477e04ccd255a91eef0d8a10c31b
dallinbeutler <dallinbeutler@gmail.com>
2017-06-02 16:01:54 -0600

    final

--- headbar.php
+++ headbar.php
@@ -4,7 +4,7 @@
                <td "id=topbar"><a  href="index.php"><img src="novacado_pit.png" alt="avacados" class="iconsize"></a></td>
 			   <td "id=topbar"><a href="assignList.php">Assignments</a></td>
 			   <td "id=topbar"><a href="Download.php">Download</a></td>
-               <td "id=topbar"><a href="Edits.php">Edits</a></td>
+               <td "id=topbar"><a href="assign5.php">Edits</a></td>
 			   <td "id=topbar"><a href="FAQs.php">FAQs</a></td>
 			   <td "id=topbar"><a href="about.php">About</a></td>
 			</tr>
