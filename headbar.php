5c512d135248ac169f0a1f374a2297fb5fad2780
dallinbeutler <dallinbeutler@gmail.com>
2017-06-04 08:22:29 -0600

    final

--- headbar.php
+++ headbar.php
@@ -1,17 +1,24 @@
-<div id="header">
- <table>
-	<tr>
-	   <td "id=topbar"><a  href="index.php"><img src="novacado_pit.png" alt="avacados" class="iconsize"></a></td>
-	   <td "id=topbar"><a href="assignList.php">Assignments</a></td>
-	   <td "id=topbar"><a href="Download.php">Download</a></td>
-	   <td "id=topbar"><a href="assign5.php">Edits</a></td>
-	   <td "id=topbar"><a href="FAQs.php">FAQs</a></td>
-	   <td "id=topbar"><a href="about.php">About</a></td>
-	   <td></td>
-	   <td></td>
-	</tr>
- </table>
-	 <div id="right">
-		<td><?php include "register.php" ?></td>
-	 </div>
-</div>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+	</head>
+	<body>
+		<div id="header">
+		 <table>
+			<tr>
+			   <td "id=topbar"><a  href="index.php"><img src="novacado_pit.png" alt="avacados" class="iconsize"></a></td>
+			   <td "id=topbar"><a href="assignList.php">Assignments</a></td>
+			   <td "id=topbar"><a href="Download.php">Download</a></td>
+			   <td "id=topbar"><a href="assign5.php">Edits</a></td>
+			   <td "id=topbar"><a href="FAQs.php">FAQs</a></td>
+			   <td "id=topbar"><a href="about.php">About</a></td>
+			   <td></td>
+			   <td></td>
+			</tr>
+		 </table>
+			 <div id="right">
+				<td><?php include "register.php" ?></td>
+			 </div>
+		</div>
+	</body>
+</html>
