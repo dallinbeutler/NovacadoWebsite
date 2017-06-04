d2b2cb5c0c712c0edba6de7015bdd75a03ba2c87
dallinbeutler <dallinbeutler@gmail.com>
2017-06-04 13:36:12 -0600

    final

--- headbar.php
+++ headbar.php
@@ -7,14 +7,12 @@
 			<div align="left">
 				<table>
 					<tr>
-					   <td "id=topbar"><a  href="index.php"><img src="novacado_pit.png" alt="avacados" class="iconsize"></a></td>
-					   <td "id=topbar"><a href="assignList.php">Assignments</a></td>
-					   <td "id=topbar"><a href="Download.php">Download</a></td>
-					   <td "id=topbar"><a href="assign5.php">Edits</a></td>
-					   <td "id=topbar"><a href="FAQs.php">FAQs</a></td>
-					   <td "id=topbar"><a href="about.php">About</a></td>
-					   <td></td>
-					   <td></td>
+					   <td><a  href="index.php"><img src="novacado_pit.png" alt="avacados" class="iconsize"></a></td>
+					   <td><a href="assignList.php">Assignments</a></td>
+					   <td><a href="Download.php">Download</a></td>
+					   <td><a href="assign5.php">Edits</a></td>
+					   <td><a href="FAQs.php">FAQs</a></td>
+					   <td><a href="about.php">About</a></td>
 					</tr>
 				</table>
 			</div>
