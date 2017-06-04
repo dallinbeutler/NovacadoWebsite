6195a83d7a73498c5fcba9f8b7999dc19287558f
dallinbeutler <dallinbeutler@gmail.com>
2017-06-04 11:28:19 -0600

    final

--- submitSignUp.php
+++ submitSignUp.php
@@ -1,12 +1,3 @@
-<?php 
-$email = htmlspecialchars($_POST['email']);
-$username = htmlspecialchars($_POST['username']);
-$msg = 'Hello, ' . $username . '! Please click the following link to validate your account:
- <a href="assign5.php">Edits list.</a>';
-
-
-mail($email,"Novacado Email Confirmation",$msg);
-?>
 <HTML>
 	<head>
     <link rel="stylesheet" type="text/css" href="mystyle.css">
@@ -17,6 +8,16 @@ mail($email,"Novacado Email Confirmation",$msg);
 		<?php include 'headbar.php'; ?>
 	    <div id="centerArea">
 	    <div id="mainArea">
-        <myp class="center"><h2>Check your email for confirmation!</h2></myp>	
+        <myp class="center"><h2>Check 
+
+			<?php 
+			$email = htmlspecialchars($_POST['email']);
+			$username = htmlspecialchars($_POST['username']);
+			$msg = 'Hello, ' . $username . '! Please click the following link to validate your account:
+			 <a href="assign5.php">Edits list.</a>';
+			mail($email,"Novacado Email Confirmation",$msg);
+			echo $email;
+			?>
+ for confirmation!</h2></myp>	
 	</body>
 </HTML>
