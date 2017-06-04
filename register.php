d472c49824cb958e759b6616583ef63536ccf92a
dallinbeutler <dallinbeutler@gmail.com>
2017-06-04 10:39:06 -0600

    final

--- register.php
+++ register.php
@@ -4,6 +4,7 @@
 
 <HTML>
 	<head>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script>
 			// Get the modal
 			var modal = document.getElementById('id01');
