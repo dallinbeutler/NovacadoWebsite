7f289911e3310c610299ff3f4867e24c65235abc
dallinbeutler <dallinbeutler@gmail.com>
2017-06-04 16:43:34 -0600

    final

--- assign5.php
+++ assign5.php
@@ -33,7 +33,69 @@
 		}
 	}
 	</script>
-	    <script type="text/javascript">
+
+   </head>
+   <body>
+      <?php include 'headbar.php'; ?>
+	  <div id="centerArea">
+	     <div id="mainArea">
+            <myp id="title" class="center"><h2>Uploads</h2></myp>		
+			<?php
+				$servername = "ec2-23-21-169-238.compute-1.amazonaws.com";
+				$username = "fmtextbjvwjlcy";
+				$password = "6ac6980946253a82ad6759afe6c2828659ca889e406e9afeeacd53d34283a17c";
+				$dbname = "dmns5jadj6q0l";
+
+				//$dbconn = pg_connect("host=ec2-23-21-169-238.compute-1.amazonaws.com port=5432 dbname=dmns5jadj6q0l user=fmtextbjvwjlcy password=6ac6980946253a82ad6759afe6c2828659ca889e406e9afeeacd53d34283a17c") or die ("Try, and therefore sadness");
+				try {
+					$db = new PDO("pgsql:host=$servername;port=5432;dbname=$dbname", $username, $password);
+					$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+				} 	
+					catch (PDOException $ex) {
+					echo "Error connecting to the db. Details: $ex";
+					die();
+				}
+
+				//$query = 'SELECT * FROM account';
+				//$query = 'SELECT name, birthday, pictureUrl FROM actor a INNER JOIN movieActor ma ON a.id = ma.actorId INNER JOIN movie m ON m.id = ma.movieId WHERE m.title = :movie_title';
+				$query = 'SELECT mes.id, mes.moviedbnumber, mes.creationdate, mes.lasteditdate, mes.moviedbnumber, acc.accountname FROM movieeditset  mes INNER JOIN account acc ON acc.id = mes.account_id';
+				$statement = $db->prepare($query);
+
+				$statement->execute();
+				$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+				// Printing results in HTML
+				echo '<div id ="dblist">';
+				echo '<table id="dblist">
+						<th> Download  </td>
+						<th>movie </th>
+						<th>creation date</th>
+						<th>last modified</th>
+						<th>author</th>
+						<th>Reviews toggle</th>';
+				foreach ($results as $row) {
+					echo '<tr>'
+					. '<td><a href="error.php"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Download_alt_font_awesome.svg/768px-Download_alt_font_awesome.svg.png" alt="Download" style="width:32px;height:32px;"> </a>'
+					. '</td><td id="movietab'.$row[id]. '" onload="searchMovie('.$row[moviedbnumber] . ',movietab'.$row[id].')">'. $row[moviedbnumber] 
+					. '</td><td>'. $row[creationdate] 
+					. '</td><td>'. $row[lasteditdate]
+					. '</td><td>'. $row[accountname] 
+					. '</td><td>'
+					//.'reviews '
+					.'<label class="switch">'
+					.'<input type="checkbox" id=checkbox'.$row[id].' onclick=showreviews('. $row[id]. ')>'
+					.' <div class="slider round"></div>'
+					.'</label>'
+					. '</td></tr><tr><td colspan=5><div id=ins'.$row[id].'></div></td></tr>';
+				
+				}	
+				echo "</table>";
+				echo '</div>';
+			?>
+		 </div>
+	  </div>
+	<script type="text/javascript">
 		var asker = "none";
         // callback for successful getConfiguration call
         function configSuccessCallback(data) {
@@ -85,68 +147,6 @@
         }
 
     </script>
-   </head>
-   <body>
-      <?php include 'headbar.php'; ?>
-	  <div id="centerArea">
-	     <div id="mainArea">
-            <myp id="title" class="center"><h2>Uploads</h2></myp>		
-			<?php
-				$servername = "ec2-23-21-169-238.compute-1.amazonaws.com";
-				$username = "fmtextbjvwjlcy";
-				$password = "6ac6980946253a82ad6759afe6c2828659ca889e406e9afeeacd53d34283a17c";
-				$dbname = "dmns5jadj6q0l";
-
-				//$dbconn = pg_connect("host=ec2-23-21-169-238.compute-1.amazonaws.com port=5432 dbname=dmns5jadj6q0l user=fmtextbjvwjlcy password=6ac6980946253a82ad6759afe6c2828659ca889e406e9afeeacd53d34283a17c") or die ("Try, and therefore sadness");
-				try {
-					$db = new PDO("pgsql:host=$servername;port=5432;dbname=$dbname", $username, $password);
-					$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-				} 	
-					catch (PDOException $ex) {
-					echo "Error connecting to the db. Details: $ex";
-					die();
-				}
-
-				//$query = 'SELECT * FROM account';
-				//$query = 'SELECT name, birthday, pictureUrl FROM actor a INNER JOIN movieActor ma ON a.id = ma.actorId INNER JOIN movie m ON m.id = ma.movieId WHERE m.title = :movie_title';
-				$query = 'SELECT mes.id, mes.moviedbnumber, mes.creationdate, mes.lasteditdate, mes.moviedbnumber, acc.accountname FROM movieeditset  mes INNER JOIN account acc ON acc.id = mes.account_id';
-				$statement = $db->prepare($query);
-
-				$statement->execute();
-				$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-				// Printing results in HTML
-				echo '<div id ="dblist">';
-				echo '<table id="dblist">
-						<th> Download  </td>
-						<th>movie (placeholder)</th>
-						<th>creation date</th>
-						<th>last modified</th>
-						<th>author</th>
-						<th>Reviews toggle</th>';
-				foreach ($results as $row) {
-					echo '<tr>'
-					. '<td><a href="error.php"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Download_alt_font_awesome.svg/768px-Download_alt_font_awesome.svg.png" alt="Download" style="width:32px;height:32px;"> </a>'
-					. '</td><td id="movietab'.$row[id]. '" onload="searchMovie('.$row[moviedbnumber] . ',movietab'.$row[id].')">'. $row[moviedbnumber] 
-					. '</td><td>'. $row[creationdate] 
-					. '</td><td>'. $row[lasteditdate]
-					. '</td><td>'. $row[accountname] 
-					. '</td><td>'
-					//.'reviews '
-					.'<label class="switch">'
-					.'<input type="checkbox" id=checkbox'.$row[id].' onclick=showreviews('. $row[id]. ')>'
-					.' <div class="slider round"></div>'
-					.'</label>'
-					. '</td></tr><tr><td colspan=5><div id=ins'.$row[id].'></div></td></tr>';
-				
-				}	
-				echo "</table>";
-				echo '</div>';
-			?>
-		 </div>
-	  </div>
-	  
    </body>
 
 </HTML>
