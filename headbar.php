77c886d8d01e78f0eba54a68427ff5c4c7b2ef3b
dallinbeutler <dallinbeutler@gmail.com>
2017-06-04 08:49:11 -0600

    final

--- headbar.php
+++ headbar.php
@@ -4,18 +4,20 @@
 	</head>
 	<body>
 		<div id="header">
-			<table>
-				<tr>
-				   <td "id=topbar"><a  href="index.php"><img src="novacado_pit.png" alt="avacados" class="iconsize"></a></td>
-				   <td "id=topbar"><a href="assignList.php">Assignments</a></td>
-				   <td "id=topbar"><a href="Download.php">Download</a></td>
-				   <td "id=topbar"><a href="assign5.php">Edits</a></td>
-				   <td "id=topbar"><a href="FAQs.php">FAQs</a></td>
-				   <td "id=topbar"><a href="about.php">About</a></td>
-				   <td></td>
-				   <td></td>
-				</tr>
-			</table>
+			<div>
+				<table>
+					<tr>
+					   <td "id=topbar"><a  href="index.php"><img src="novacado_pit.png" alt="avacados" class="iconsize"></a></td>
+					   <td "id=topbar"><a href="assignList.php">Assignments</a></td>
+					   <td "id=topbar"><a href="Download.php">Download</a></td>
+					   <td "id=topbar"><a href="assign5.php">Edits</a></td>
+					   <td "id=topbar"><a href="FAQs.php">FAQs</a></td>
+					   <td "id=topbar"><a href="about.php">About</a></td>
+					   <td></td>
+					   <td></td>
+					</tr>
+				</table>
+			</div>
 			<div id="right">
 				<?php include "register.php" ?> 
 				<?php include "login.php" ?> 
