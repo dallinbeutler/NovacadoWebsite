0ecdb42e54a70b582b97e1fc164f771b05085417
dallinbeutler <dallinbeutler@gmail.com>
2017-06-04 11:17:00 -0600

    final

--- register.php
+++ register.php
@@ -32,7 +32,15 @@
 				xmlhttp.send();
 			}
 		};
-		
+
+		function checkpasswords(){
+			if (document.getElementById("pwd1").value === document.getElementById("pwd2").value){
+				document.getElementById("submitbutton").disabled = false;
+				}
+			else{
+				document.getElementById("submitbutton").disabled = true;
+				}
+		}
 		$('#regform').submit(function() {
 			alert('Handler for .submit() called.');
 			return false;
@@ -54,15 +62,15 @@
 					<input type="email" placeholder="Enter Email" name="email" required>
 					
 					<label><b>Password</b></label>
-					<input type="password" placeholder="Enter Password" name="psw" required>
+					<input id ="pwd1" type="password" placeholder="Enter Password" name="psw" required>
 
 					<label><b>Repeat Password</b></label>
-					<input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+					<input id="pwd2" type="password" placeholder="Repeat Password" name="psw-repeat" onchange="checkpasswords()" required>
 					<p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
 					<div class="clearfix">
 						<button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-						<button type="submit" class="signupbtn">Sign Up</button>
+						<button type="submit" id="submitbutton" class="signupbtn">Sign Up</button>
 					</div>
 				</div>
 			</form>
