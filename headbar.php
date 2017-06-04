09f9238ab5e484e2d590aa0c331942e4ede443c8
dallinbeutler <dallinbeutler@gmail.com>
2017-06-04 13:43:25 -0600

    final

--- headbar.php
+++ headbar.php
@@ -7,10 +7,10 @@
 			<div align="left">
 				<button onclick = "window.location = 'index.php'"><img src="novacado_pit.png" alt="avacados" class="iconsize"></button>
 				<button onclick = "window.location = 'assignList.php'"> Assignments</button>
-				<button onclick = "window.location = 'assignList.php'"> Assignments</button>
-				<button onclick = "window.location = 'assignList.php'"> Assignments</button>
-				<button onclick = "window.location = 'assignList.php'"> Assignments</button>
-				<button onclick = "window.location = 'assignList.php'"> Assignments</button>
+				<button onclick = "window.location = 'assign5.php'"> Edits</button>
+				<button onclick = "window.location = 'Download.php'"> Downloads</button>
+				<button onclick = "window.location = 'FAQs.php'">FAQs</button>
+				<button onclick = "window.location = 'about.php'"> About</button>
 			</div>
 			<div id="right" align="right">
 				<?php include "register.php" ?> 
