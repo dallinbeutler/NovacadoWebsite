e7809749cb91d72aebc2acdb09adec47ddb702a6
dallinbeutler <dallinbeutler@gmail.com>
2017-06-05 10:56:58 -0600

    final

---
+++ error.php
@@ -0,0 +1,20 @@
+<HTML>
+	<head>
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+		<link rel="icon" href="novacado_pit.png">
+		<title> Edits </title>
+    </head>
+    <body>
+    <?php include 'headbar.php'; ?>
+	<div id="centerArea">
+	    <div id="mainArea">
+			<myp id="title" class="center"><h2>Error!</h2></myp>
+			<div id= "pictureHolder"> 
+				<img src="https://anticap.files.wordpress.com/2012/11/pota-statue-of-liberty-2.jpeg" alt='baderror' class="centered" />
+			</div>
+		</div>
+	</div>
+
+   </body>
+
+</HTML>
